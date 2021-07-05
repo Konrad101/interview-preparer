1f0dc44202822cd86873cfff4a1fce0dc40d23f2
Konrad <konrad.hajduga@gmail.com>
2021-07-05 19:32:40 +0200

    Initial commit

---
+++ src/main/java/pl/interviewhelpers/interviewpreparer/InterviewPreparerApplication.java
@@ -0,0 +1,13 @@
+package pl.interviewhelpers.interviewpreparer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPreparerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPreparerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/interviewhelpers/interviewpreparer/InterviewPreparerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.interviewhelpers.interviewpreparer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPreparerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
